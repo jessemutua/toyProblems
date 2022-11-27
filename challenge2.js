d6b6a34405823edf37127adcb2abdb618f678947
Jesse <jessemutua76@gmail.com>
2022-11-27 10:10:03 +0300

    Added an htnl file to collect grade

---
+++ challenge2.js
@@ -0,0 +1,14 @@
+//Write a function that prompts the user to input student marks.
+// The input should be between 0 and 100. 
+//The output should correspond the correct grade, as shown below: 
+//A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+let marks = (marks) => {
+    console.log(marks)
+}
+if (marks >= 0 || marks <= 100) {
+    console.log("Invalid!")
+} else {
+    console.log("Valid")
+}
+marks(223)
